4b49e655375fda71cb862baea98def3355a0ecd1
Bhavin6497 <bhavinkamaliya95@gmail.com>
2025-04-25 08:09:37 +0530

    Final_Cutoff_Streamlit.py

--- Final_Cutoff_Streamlit.py
+++ Final_Cutoff_Streamlit.py
@@ -266,9 +266,9 @@ def page0():
                 # Sort the list of files alphabetically
                 files = sorted(session_state.files, key=lambda x: x.name)
                 
-                porosity_options = ['PIGE_QEPP', 'PIGE', 'PHIE_F', 'PHIE', 'PIGE_F','PHIE_Final','PHIE_FINAL']
-                saturation_options = ['SUWI', 'SW_F', 'SW', 'SUWI_QEPP', 'sw', 'Sw_F','SWE','SWE_Final','SWE_FINAL']
-                vcl_options = ['VCL', 'VCL_F', 'VCL_QEPP', 'VCL_GEO_QEPP','VSH','VSH_Final','VSH_FINAL']
+                porosity_options = ['PIGE_QEPP', 'PIGE', 'PHIE_F', 'PHIE', 'PIGE_F','PHIE_Final','PHIE_FINAL','PIGE_RA','PIGN_QEPP']
+                saturation_options = ['SUWI', 'SW_F', 'SW', 'SUWI_QEPP', 'sw', 'Sw_F','SWE','SWE_Final','SWE_FINAL','SUWI_RA']
+                vcl_options = ['VCL', 'VCL_F', 'VCL_QEPP', 'VCL_GEO_QEPP','VSH','VSH_Final','VSH_FINAL','VCL_RA','Shale_QEPP']
                 depth_options = ['DEPT', 'MD', 'DEPTH']
                 
                 session_state.dfs = {}  # Initialize the dfs dictionary in session_state
